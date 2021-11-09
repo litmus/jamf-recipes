177043eab9ffc5a4a75505b761b3534fd908b189
DJ Delisser <dj@djm1pro.local>
2021-11-09 11:44:13 -0500

    updated identifiers, parent & child recipes to reflect repo change

--- Scripts/run_autopkg.sh
+++ Scripts/run_autopkg.sh
@@ -3,7 +3,7 @@
 # This script will be setup to run on a scheduled basis via cron job on the Litmus Cambridge Mac Mini
 # The schedule is below: 
 # Daily @ 8am EST, Monday - Friday, crontab below
-# 0 8 * * 1-5 /Volumes/JAMF/Autopkg/RecipeRepos/com.github.litmus.itoperations.jamf-recipes/Scripts/run_autopkg.sh
+# 0 8 * * 1-5 /Volumes/JAMF/Autopkg/RecipeRepos/com.github.litmus.jamf-recipes/Scripts/run_autopkg.sh
 # Logs output to Log folder for Autopkg
 # NOTE: IF NEW RECIPES ARE ADDED, RECIPE_LIST WILL NEED TO BE UPDATED
 
@@ -11,7 +11,7 @@
 /usr/local/bin/autopkg repo-update all
 
 #Run the Recipe List
-/usr/local/bin/autopkg run --recipe-list /Volumes/JAMF/AutoPkg/RecipeRepos/com.github.litmus.itoperations.jamf-recipes/recipe_list.plist
+/usr/local/bin/autopkg run --recipe-list /Volumes/JAMF/AutoPkg/RecipeRepos/com.github.litmus.jamf-recipes/recipe_list.plist
 
 exit 0 #Success
 exit 1 #Failure
